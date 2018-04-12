99b16aa5b319a8c9934584423395afd10eb92429
苏福良 <apple@>
2018-04-12 14:45:53 +0800

    有字的

--- 统一封装版本/Request/HttpManager.swift
+++ 统一封装版本/Request/HttpManager.swift
@@ -26,6 +26,7 @@ class HttpManager: NSObject {
         manager.responseSerializer.acceptableContentTypes = nil
         manager.securityPolicy = securityPolicy
         
+        let s = 2
     }
     
     func post(interface:String, param:Dictionary<String,Any>, success:@escaping successBlock, failur:@escaping failurBlock) -> Void {
