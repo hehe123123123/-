24e747c9dbe4f731893306644fcfa43c964865d1
苏福良 <apple@>
2018-04-12 14:43:44 +0800

    测试 请空

--- 统一封装版本/Request/HttpManager.swift
+++ 统一封装版本/Request/HttpManager.swift
@@ -26,7 +26,6 @@ class HttpManager: NSObject {
         manager.responseSerializer.acceptableContentTypes = nil
         manager.securityPolicy = securityPolicy
         
-        let ori = 5
     }
     
     func post(interface:String, param:Dictionary<String,Any>, success:@escaping successBlock, failur:@escaping failurBlock) -> Void {
