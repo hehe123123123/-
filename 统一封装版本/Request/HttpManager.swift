c6280d96bbf0566a6f6c51e8487a2241896c7db8
苏福良 <apple@>
2018-04-12 15:17:24 +0800

    远端更新

--- 统一封装版本/Request/HttpManager.swift
+++ 统一封装版本/Request/HttpManager.swift
@@ -27,6 +27,7 @@ class HttpManager: NSObject {
         manager.securityPolicy = securityPolicy
         
         let s = 2
+        let gitPull = 10
     }
     
     func post(interface:String, param:Dictionary<String,Any>, success:@escaping successBlock, failur:@escaping failurBlock) -> Void {
